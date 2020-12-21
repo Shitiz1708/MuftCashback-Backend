7ab2531df682b65b666ac9ce625499e43ae6b762
Shitiz Gupta <guptashitiz17@gmail.com>
2020-12-21 17:34:21 +0530

    initial commit

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -2,14 +2,39 @@
 
 const { Telegraf } = require('telegraf');
 
+
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 const bot = new Telegraf('1439119247:AAFhbFCsa9LkVN4x9f-Gq549z4GSNQ_mI8s')
 
 bot.start((ctx)=>{
-    return ctx.reply('Hello There');
+    return ctx.reply('Hello There! Please register to the bot using /register <email>');
+})
+
+bot.command('register',(ctx)=>{
+    var msg = ctx.message.text
+    var email = msg.substring(10);
+    if(validateEmail(email)==false){
+        return ctx.reply('Please Enter Valid Email')
+    }
+
+    const params = {
+        TableName:'Users',
+        Key:{
+            SubId:subid
+        },
+        ProjectionExpression:
+    }
 })
 
 bot.on('text',(ctx)=>{
-    return ctx.reply('You entered some text');
+    return ctx.reply("HEY");
 })
 
 module.exports = {
