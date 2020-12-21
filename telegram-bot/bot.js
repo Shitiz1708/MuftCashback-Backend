1330f7bc37bbe9c9422726061c451b284bfde495
Shitiz Gupta <guptashitiz17@gmail.com>
2020-12-21 14:43:15 +0530

    initial commit

---
+++ telegram-bot/bot.js
@@ -0,0 +1,17 @@
+// This file contains all bot functions
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('1439119247:AAFhbFCsa9LkVN4x9f-Gq549z4GSNQ_mI8s')
+
+bot.start((ctx)=>{
+    return ctx.reply('Hello There');
+})
+
+bot.on('text',(ctx)=>{
+    return ctx.reply('You entered some text');
+})
+
+module.exports = {
+    bot
+}
--- telegram-bot/handler.js
+++ telegram-bot/handler.js
@@ -1,18 +1,59 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { bot } = require('./bot.js')
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const getResponseHeaders = () => {
+  return {
+      'Access-Control-Allow-Origin': '*'
+  };
+}
+
+
+module.exports.hello = async event => {
+  try{
+    const body = JSON.parse(event.body);
+    console.log(body)
+
+    await bot.handleUpdate(body)
+
+    return {
+      statusCode:200,
+      headers:getResponseHeaders(),
+      body:JSON.stringify(
+        {
+          message:'OKAY'
+        }
+      )
+    };
+  }catch(err){
+    console.log(err);
+    return err;
+  }
+
 };
+
+module.exports.setWebhook = async event => {
+  try {
+
+      var url = 'https://' + event.headers.Host + '/' + event.requestContext.stage + '/webhook';
+
+      await bot.telegram.setWebhook(url);
+
+      return {
+          statusCode: 200,
+          headers: getResponseHeaders(),
+          body: JSON.stringify({url: url})
+      };
+
+  } catch (err) {
+      console.log("Error: ", err);
+      return {
+          statusCode: err.statusCode ? err.statusCode : 500,
+          headers: getResponseHeaders(),
+          body: JSON.stringify({
+              error: err.name ? err.name : "Exception",
+              message: err.message ? err.message : "Unknown error"
+          })
+      };
+  }
+}
