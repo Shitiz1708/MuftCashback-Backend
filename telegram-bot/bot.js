77e891a1bc2f426a5e4dd1a3acf31a83d65a3513
Shitiz Gupta <guptashitiz17@gmail.com>
2020-12-30 18:32:28 +0530

    initial commit

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -55,9 +55,11 @@ const getCompleteUsertable = async() =>{
 
 }
 
-const alreadyRegistered = async(email) =>{
-    const allUsers = getCompleteUsertable()
+const alreadyRegistered = async (email) =>{
+    const allUsers = await getCompleteUsertable()
+    console.log(allUsers)
     for(var i=0;i<allUsers.length;i++){
+        console.log("Inside Loop")
         if(allUsers[i]['Email']==email && "BotEnabled" in allUsers[i]){
             console.log('User Found')
             return true
@@ -80,12 +82,87 @@ const shortenLink = async(url) =>{
                 'Content-Type': 'application/json'
             }
         })
-        console.log(response['link'])
-        return response['link']
+        console.log(response['data']['link'])
+        return response['data']['link']
     }catch(err){
         console.log(err)
         return err
     }
+}
+
+const unshortenLink = async(link) =>{
+    try{
+        var response = await axios.get(link)
+        return response.request.res.responseUrl
+    }catch(err){
+        console.log(err)
+        return ctx.reply('Link cannot be unshorten')
+    }
+}
+
+const merchantFlipkart = (link,user) =>{
+    //2. For Flipkart
+    //2.1 Remove cmpid,affid,affExtparam1
+    //2.2 Add personal affid and affExtParam as User Subid
+    //2.3 Replace www by dl
+    var listOfStrings = link.split("&")
+    for(var i=0;i<listOfStrings.length;i++){
+        if(listOfStrings[i].includes('cmpid') || listOfStrings[i].includes('affid') || listOfStrings[i].includes('affExtParam1') || listOfStrings[i].includes('affExtParam2')){
+            listOfStrings.splice(i,1)
+        }else if(listOfStrings[i].includes('www')){
+            listOfStrings[i].replace('www','dl')
+        }
+    }
+    listOfStrings.push('affid=bansalsid')
+    listOfStrings.push('affExtParam1='+user['SubId'])
+    var affLink = listOfStrings.join('&')
+    return affLink
+}
+
+const merchantAmazon = (link,user) =>{
+    if('AmazonEnabled' in user){
+        var idx = link.indexOf("tag=")
+        var affLink = null
+        console.log(idx)
+        if(idx!=-1){
+            var tempstr = ""
+            for(var i=idx+4;i<link.length;i++){
+                if(link[i]=='&'){
+                    break
+                }
+                tempstr+=link[i]
+            }
+            console.log(tempstr)
+            affLink = link.replace(tempstr,user['SubId']+'-21')
+        }else{
+            affLink = link+"?tag="+user['SubId']+'-21'
+        }
+        return affLink
+    }else{
+        return ctx.reply('Please register on our app for using amazon affiliate')
+    }
+}
+
+const checkMerchant = (link) =>{
+    //1. Check for merchant
+    if(link.includes("flipkart")){
+        return "flipkart"
+    }else if(link.includes("amazon")){
+        return "amazon"
+    }else{
+        return "third-party"
+    }
+}
+
+const createAffLink = (link,user) =>{
+    var merchant = checkMerchant(link)
+    var afflink = null
+    if(merchant=='flipkart'){
+        afflink = merchantFlipkart(link,user)
+    }else if(merchant=='amazon'){
+        afflink = merchantAmazon(link,user)
+    }
+    return afflink
 }
 
 
@@ -104,7 +181,8 @@ bot.command('register',async (ctx)=>{
     }
 
     // If email is already registered as referrer . Break the operation
-    var alreadyUser = alreadyRegistered(email)
+    var alreadyUser = await alreadyRegistered(email)
+    console.log(alreadyUser)
     if(alreadyUser){
         console.log('Inside this')
         return ctx.reply("Email Already Registered. Please contact support for help!")
@@ -137,14 +215,16 @@ bot.command('register',async (ctx)=>{
 
 })
 
-bot.on('text',(ctx)=>{
+bot.on('text',async(ctx)=>{
     //Check if a person with this chat id is registered or not
     const chat_id = ctx.chat.id
-    var allUsers = getCompleteUsertable()
+    const allUsers = await getCompleteUsertable()
+    console.log(allUsers)
     var currUser = null
     for(var i=0;i<allUsers.length;i++){
         if(allUsers[i]['UserBotId']==chat_id){
             currUser = allUsers[i]
+            console.log('User Found')
         }
     }
 
@@ -152,17 +232,23 @@ bot.on('text',(ctx)=>{
         return ctx.reply('User Not Found! Please Register to our app and use the same email to register to this bot')
     }
 
-    //Check if the link is shortened or not
+    var vanillaLink = ctx.message.text
+    console.log(vanillaLink)
 
-
-    //Add affid to the link
+    //unshorten the link
+    var unshortedLink = await unshortenLink(vanillaLink)
+    console.log(unshortedLink)
+    
+    //Create Affiliate Link
+    var afflink = createAffLink(unshortedLink,currUser)
+    console.log(afflink)
 
     //Shorten the link
-    var link = shortenLink('https://www.amazon.in/Ambrane-PP-150-15000mAH-Lithium-Polymer/dp/B07MHMKSXZ?ref_=Oct_DLandingS_D_c7cbd4d0_61&smid=A14CZOWI0VEHLG')
-    // var link = 'https://www.amazon.in/Ambrane-PP-150-15000mAH-Lithium-Polymer/dp/B07MHMKSXZ?ref_=Oct_DLandingS_D_c7cbd4d0_61&smid=A14CZOWI0VEHLG'
+    var shortlink = await shortenLink(afflink)
+    console.log(shortlink)
 
     //Return the link
-    return ctx.reply(link)
+    return ctx.reply(shortlink)
 })
 
 module.exports = {
